4728e614bcc6fb077fed8fa3b381da30889bbee2
Go-Maun <gomaun55@gmail.com>
2019-09-25 18:58:48 -0700

    reformatted base_conversion.py

--- Lab03/base_conversion.py
+++ Lab03/base_conversion.py
@@ -1,32 +1,54 @@
 def base_conversion():
-    """Calculates what the user inputs into any base between 2 - 9 the user requests"""
-    goodbye = 0
+    """asks the user for a base and a number"""
     base = int(input("Choose your desired base between 2 and 9: "))
     destination = (base**4)-1
     number = int(input("What number between 0 and " + str(destination) + " would you like to convert?: "))
-    original_number = number
-
     if 0 <= number <= destination:
-
-        place_1 = number % base
-        number = number//base
-
-        place_2 = number % base
-        number = number//base
-
-        place_3 = number % base
-        number = number//base
-
-        place_4 = number % base
-        number = number//base
-
-        converted = str(place_4) + str(place_3) + str(place_2) + str(place_1)
-
-        print("Your number", original_number, "in base", base, "is", int(converted))
-
+        calculations(base, number)
     else:
+        wrong_input(destination)
 
-        print("I said between 0 and " + str(destination) + ", you clearly cant follow instructions, goodbye!")
+
+def calculations(x, y):
+    """calculates and prints the converted number"""
+    original_number = y
+    place_1 = remainder(x, y)
+    y = new_divisor(x, y)
+    place_2 = remainder(x, y)
+    y = new_divisor(x, y)
+    place_3 = remainder(x, y)
+    y = new_divisor(x, y)
+    place_4 = remainder(x, y)
+    y = new_divisor(x, y)
+    end_result(x, original_number, place_4, place_3, place_2, place_1)
+
+
+def remainder(x, y):
+    """finds the remainder number/base"""
+    place = y % x
+    return place
+
+
+def new_divisor(x, y):
+    """calculates a new value for the number variable"""
+    number = y//x
+    return number
+
+
+def string_concatenation(a, b, c, d):
+    """concatenates the new numbers together"""
+    final = str(a) + str(b) + str(c) + str(d)
+    return final
+
+
+def end_result(x, y, a, b, c, d):
+    """prints the end result"""
+    print("Your number", y, "in base", x, "is", int(string_concatenation(a, b, c, d)))
+
+
+def wrong_input(max):
+    """tells the user that they input an invalid number"""
+    print("I said between 0 and " + str(max) + ", you clearly cant follow instructions, goodbye!")
 
 
 base_conversion()
